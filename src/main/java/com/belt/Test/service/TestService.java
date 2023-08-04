ee9003562aa305375fb20d856b7d255c19c143a8
LiryelBeloAguiar <liryel.belo.702@ufrn.edu.br>
2023-08-04 13:01:24 -0300

    Primeiro Commit

---
+++ src/main/java/com/belt/Test/service/TestService.java
@@ -0,0 +1,48 @@
+package com.belt.Test.service;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.belt.Test.model.TestModel;
+import com.belt.Test.repository.TestRepository;
+
+@Service
+public class TestService {
+    private TestRepository testRepository;
+
+    @Autowired
+    public TestService(TestRepository testRepository) {
+        this.testRepository = testRepository;
+    }
+
+    public List<TestModel> create(TestModel testModel) {
+        testRepository.save(testModel);
+        return list();
+    }
+
+    public List<TestModel> list() {
+        return testRepository.findAll();
+    }
+
+    public List<TestModel> update(TestModel testModel) {
+        testRepository.save(testModel);
+        return list();
+    }
+
+    public TestModel getById(Long id) {
+        return testRepository.getById(id);
+    }
+
+    public Optional<TestModel> findById(Long id) {
+        return testRepository.findById(id);
+    }
+
+    public List<TestModel> delete(Long id) {
+        testRepository.deleteById(id);
+        return list();
+    }
+
+}
